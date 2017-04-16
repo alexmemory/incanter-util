f79bb1b368778216f990b518eb7350df304d5025
Alex Memory <alexmemory@gmail.com>
2017-04-16 08:32:59 -0700

    Update version. Add URL.

--- project.clj
+++ project.clj
@@ -1,10 +1,11 @@
-(defproject org.clojars.alexmemory/incanter-util "0.1.0-SNAPSHOT"
+(defproject org.clojars.alexmemory/incanter-util "0.1.0"
   :description "Utilities for Incanter"
-  :dependencies [;; [org.clojure/clojure "1.8.0"]
-                 [org.clojars.alexmemory/clj-util "0.1.0-SNAPSHOT"]
-                 [incanter/incanter-core "1.5.6"
-                  ;; Interferes with PSL, IIRC
-                  ;; :exclusions [net.sourceforge.jplasma/jplasma]
-                  ]
-                 [incanter/incanter-io "1.5.6"]
-                 ])
+  :url "https://github.com/alexmemory/incanter-util"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/data.csv "0.1.3"]
+                 [org.clojars.alexmemory/clj-util "0.1.0"]
+                 [incanter/incanter-core "1.5.6"]
+                 [incanter/incanter-io "1.5.6"]]
+  :aot :all
+  :pedantic :warn                       ; Class path warnings
+  )
